7718b437dfa17a690bd324595417fca8f1282a12
Colton Blake <cltnblk@gmail.com>
2019-03-02 14:32:20 -0500

    limited comment size

--- berried_treasure/berriedtreasureapp/models.py
+++ berried_treasure/berriedtreasureapp/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Entry(models.Model):
     name = models.CharField(max_length=12)
     date = models.DateTimeField(auto_now=True)
-    comment = models.TextField(max_length=280)
+    comment = models.TextField(max_length=80)
     
