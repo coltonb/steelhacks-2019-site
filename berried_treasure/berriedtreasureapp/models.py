c9a6241b5029177668682d94d5fdfb6a8ffe5dff
aro24 <aro24@pitt.edu>
2019-03-01 21:26:26 -0500

    sql working???????

--- berried_treasure/berriedtreasureapp/models.py
+++ berried_treasure/berriedtreasureapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Entry(models.Model):
+    name = models.CharField(max_length=12)
+    date = models.DateField(auto_now=True)
+    comment = models.TextField(max_length=280)
