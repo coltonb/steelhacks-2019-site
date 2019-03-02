39f02340a72815dbc5c80ffc9d9641092bdd29c1
Colton Blake <cltnblk@gmail.com>
2019-03-01 23:02:13 -0500

    Forms working

--- berried_treasure/berried_treasure/urls.py
+++ berried_treasure/berried_treasure/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from berriedtreasureapp import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'results', views.results),
+    url(r'', views.index)
 ]
