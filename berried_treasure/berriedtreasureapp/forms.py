b8f8a309427ec521b979108bfa694d8f19e0558c
aro24 <aro24@pitt.edu>
2019-03-02 11:15:24 -0500

    pretty pooch

--- berried_treasure/berriedtreasureapp/forms.py
+++ berried_treasure/berriedtreasureapp/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class NameForm(forms.Form):
     name = forms.CharField(label = 'Your Name', max_length=12)
-    comment = forms.CharField(label = 'Your Comment', max_length=200)
+    comment = forms.CharField(label = 'Your Comment', max_length=280, widget=forms.Textarea)
